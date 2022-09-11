e999d9d3351d589b0edbfebfc7e1e310020c462c
RaulOrtiz99 <raulalbertoortiz290199@hotmai.com>
2022-09-11 02:41:49 -0400

    agregando la vista de chat, o sea donde se veran todos los chats de la app

--- lib/routes/pages/chat_page.dart
+++ lib/routes/pages/chat_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
@@ -6,8 +8,50 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('ChatPage'),
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 1,
+        backgroundColor: Colors.white,
+        title: Column(
+          children: [
+            Center(
+              child: CircleAvatar(
+                maxRadius: 14,
+                child: Text(
+                  'Te',
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+                backgroundColor: Colors.blue[100],
+              ),
+            ),
+            SizedBox(height: 3),
+            Text(
+              "Melissa Flores",
+              style: TextStyle(
+                color: Colors.black87,
+                fontSize: 12,
+              ),
+            )
+          ],
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                itemBuilder: (_, i) => Text('$i'),
+                reverse: true,
+              ),
+            ),
+            Divider(
+              height: 1,
+            ),
+          ],
+        ),
       ),
     );
   }
